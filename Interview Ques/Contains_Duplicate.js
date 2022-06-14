fe361fd50f9b467498812697ef331ad06abe4893
Tanish <tanishchaurasia6@gmail.com>
2022-06-14 19:54:01 +0530

    New ques

--- Interview Ques/Contains_Duplicate.js
+++ Interview Ques/Contains_Duplicate.js
@@ -14,7 +14,7 @@
 // Output: true
 
 
-var containsDuplicate = function(nums) {
+const containsDuplicate = function(nums) {
     nums.sort((a,b) => (a-b))
     for (let i=0; i<nums.length; i++){
        if (nums[i]===nums[i+1]){
